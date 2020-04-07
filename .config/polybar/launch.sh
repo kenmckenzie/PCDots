715844cdfb7b8916d6e38369c0a94f37f68f2ade
Ken McKenzie <slain.12@gmail.com>
2020-04-07 23:14:44 +0200

    Misc changes, not in use currently

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,5 +9,5 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch bar1 and bar2 , Will also start a bar for vnc when active 
 polybar bar1 &
 polybar bar2 &
-polybar vncbar &
+#polybar vncbar &
 echo "Bars launched..."
