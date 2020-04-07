2cade29a840cfde16cd43999dc56d8e8589f8fc5
Ken McKenzie <slain.12@gmail.com>
2020-04-07 23:12:42 +0200

    Removing unused files

--- .conky/Archbar/Conkypanel/startconky.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-current="$(readlink -f $(dirname "$0"))"
-previous="$(pwd)"
-cd "$current"
-conky -c ./ConkyMain
-#conky -c ./rings &
-#conky -c ./cpu &
-# conky -c ./mem &
-#conky -c ./gmail
